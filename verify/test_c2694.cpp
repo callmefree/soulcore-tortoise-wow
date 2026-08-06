a0fcf20bce3a9df946b79e93b8b649fec26f0b95
callmefree <674968117@qq.com>
2026-08-06 09:38:38 +0800

    Repro v2: mimic real trigger (user-declared dtor in base template) + MSVC default C++14 mode

--- verify/test_c2694.cpp
+++ verify/test_c2694.cpp
@@ -1,20 +1,38 @@
-// 最小复现: 模拟 tortoise-wow WorldSocketMgr.cpp 对 ACE_Singleton 的使用
-// MSVC 对模板类隐式析构的 noexcept 推断(noexcept(false))与 ACE_Cleanup 基类
-// 析构(=default, noexcept(true))不匹配时触发 C2694
+// 最小复现 v2: 模拟真实触发条件
+// 真实场景: WorldSocketMgr : MangosSocketMgr<WorldSocket> (基类有用户声明的析构)
+//           WorldSocketMgr.cpp 用 ACE_Singleton<WorldSocketMgr, ACE_Thread_Mutex>
+// 编译模式: MSVC 默认 /std:c++14 (CMake 未指定 CXX_STANDARD 时), 非 c++17
 #include <ace/Singleton.h>
 #include <ace/Thread_Mutex.h>
 
-class MockWorldSocketMgr
+// 模拟 MangosSocketMgr: 模板基类, 有用户声明的析构函数(声明+定义分离)
+template <typename T>
+class MockMgrBase
 {
 public:
-    MockWorldSocketMgr() {}
-    ~MockWorldSocketMgr() {}
+    MockMgrBase();
+    ~MockMgrBase();
+
+    T* m_ptr;
+};
+
+template <typename T>
+MockMgrBase<T>::MockMgrBase() : m_ptr(0) {}
+
+template <typename T>
+MockMgrBase<T>::~MockMgrBase() { delete m_ptr; }
+
+// 模拟 WorldSocketMgr: 继承带用户析构的模板基类, 被 ACE_Singleton 包装
+class WorldSocketMgrLike : public MockMgrBase<int>
+{
+public:
+    WorldSocketMgrLike() {}
     int dummy() { return 42; }
 };
 
-MockWorldSocketMgr* GetMgr()
+WorldSocketMgrLike* GetMgr()
 {
-    return ACE_Singleton<MockWorldSocketMgr, ACE_Thread_Mutex>::instance();
+    return ACE_Singleton<WorldSocketMgrLike, ACE_Thread_Mutex>::instance();
 }
 
 int main() { return GetMgr() ? 0 : 1; }
